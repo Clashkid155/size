e1793fbac3f169c6041c20abb76836062724f960
Clashkid155 <frankdiss9@gmail.com>
2021-10-29 11:03:33 +0100

    Solve not found file

--- linux/include/size_plugin.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-//using std::string;
-
-extern "C" int64_t getAvailableDiskSpace(const char *path);
-extern "C" int64_t getFreeDiskSpace(const char* path);
-extern "C" int64_t getDiskCapacity(const char* path);
-extern "C" int64_t empty(const char *path);
